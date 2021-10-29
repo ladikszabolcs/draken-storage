18b9a80f274abe06442751d3140ba124a880e02b
Szabolcs Viktor Ladik <ladikszabolcs@draken.hu>
2021-10-29 18:24:04 +0200

    alap viewk

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ require "controllers/class_controllers.php";
 require "models/class_models.php";
 require "views/class_views.php";
 
-echo "Hello!";
-
 $database = new Database();
+$view = new Views();
+$view->menuView();
+$view->loginView();
+
 $database->connect("LOCAL");
 
-$database->sqlquery("SHOW TABLES;");
-
-echo "Mizu?";
+var_dump($_POST);
 
 ?>
