2cc0fe342474cacc87484baed3679b23c3ac1264
Szabolcs Viktor Ladik <ladikszabolcs@draken.hu>
2021-11-12 16:57:50 +0100

    Update login.php

--- login.php
+++ login.php
@@ -5,10 +5,13 @@ require "models/class_models.php";
 require "views/class_views.php";
 
 $database = new Database();
+$database->connect("LOCAL");
+// 							   SELECT * FROM users WHERE email="ladikszabolcs@draken.hu"
+$result = $database->sqlquery("SELECT * FROM users WHERE email=\"" . $_POST["username"] . "\"");
 
-$result = $database->sqlquery("SELECT * FROM users WHERE email=" . $_POST["username"]);
+//var_dump("SELECT * FROM users WHERE email=" . $_POST["username"]);
 
-echo($result);
+var_dump($result);
 
 var_dump($_POST);
 
