5135806061e6337064f71abf098061346cdf28d9
t-robert80 <t-robert@outlook.hu>
2021-11-19 16:45:52 +0100

    regisztráció

---
+++ registry.php
@@ -0,0 +1,40 @@
+<?php
+
+require "views/class_views.php";
+require "models/class_models.php";
+require "controllers/class_controllers.php";
+
+var_dump($_POST);
+echo("<br>");
+
+$database = new Database();
+$database->connect("LOCAL");
+
+
+
+#echo($_POST["username"]);
+echo("<br>");
+#echo("INSERT INTO `users` (`id`, `email`, `password`) VALUES (NULL, \"" . $_POST["username"] . "\", \"" . $_POST["password"] . "\")");
+echo("<br>");
+
+
+
+$result = $database->sqlquery("INSERT INTO `users` (`id`, `email`, `password`) VALUES (NULL, \"" . $_POST["username"] . "\", \"" . $_POST["password"] . "\")");
+
+
+
+
+
+#var_dump($result);
+echo("<br>");
+
+
+#var_dump($_SESSION);
+echo("<br>");
+
+
+
+#header("location: index.php");
+?>
+
+
--- views/class_views.php
+++ views/class_views.php
@@ -1,17 +1,15 @@
 <?php
 
-class Views 
-{
-
+class Views {
 	function __construct()
 	{
 		echo('
 			<!DOCTYPE html>
 			<html lang="en">
 			<head>
-  			<title>Draken-Storage</title>
-  			<meta charset="utf-8">
-  			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<title>Draken-Storage</title>
+		  	<meta charset="utf-8">
+		  	<meta name="viewport" content="width=device-width, initial-scale=1">
   			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
   			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
 			</head>
@@ -19,20 +17,35 @@ class Views
 			');
 	}
 
-	function menuView() 
+	function menuView()
 	{
-		echo('
+		echo ('
 			<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
   			<div class="container-fluid">
-    		<a class="navbar-brand" href="index.php">Storage</a>
-    		<ul class"nav navbar-nav">
-  				<li class="nav-item">
-  				<form action="logout.php" method="post">
-  					<button type="submit" name="logout" class="btn btn-primary">Kijelentkezés</button>
-  				</form>
-  				</li>
-  			</ul>
-  			</div>
+  			<a class="navbar-brand" href="index.php">Storage</a>
+  			
+		
+
+
+			<ul class"nav navbar-nav">
+				<li class="nav-item">
+					<form action="index.php" method="post">
+						<button type="submit" name="registry" class="btn btn-primary">Regisztráció</button>
+					</form>
+				</li>
+			</ul>
+
+
+
+
+			<ul class"nav navbar-nav">
+				<li class="nav-item">
+					<form action="logout.php" method="post">
+						<button type="submit" name="logout" class="btn btn-primary">Kijelentkezés</button>
+					</form>
+				</li>
+			</ul>
+			</div>
 			</nav>
 			');
 	}
@@ -40,29 +53,46 @@ class Views
 	function loginView()
 	{
 		echo('
-			<div style="margin-top:80px" class="container-fluid">
-				<center>
-					<form action="login.php" method="post">
-						<label for="username" class="form-label">Felhasználónév:</label>
-						<input id="username" name="username" class="form-control" type="text">
+			 <div style="margin-top:80px" class="container-fluid">
+			 	<center>
+			 		<form action="login.php" method="post">
+			 			<label for="username" class="form-label">Felhasználónév</label>
+			 			<input id="username" name="username" class="form-control "type="text">
+			 			<label for="password" class="form-label">Jelszó</label>
+			 			<input id="password" name="password" class="form-control "type="password">
+			 			<button type="submit" name="login" class="btn btn-primary">Belépés</button>
+			 		</form>
+			 	</center>
+			 </div>
+			');
+	}
 
-						<label for="password" class="form-label">Jelszó:</label>
-						<input id="password" name="password" class="form-control" type="password">
-
-						<button type="submit" name="login" class="btn btn-primary">Belépés</button>
-					</form>
-				</center>
-			</div>
+	function registryView()
+	{
+		echo('
+			 <div style="margin-top:80px" class="container-fluid">
+			 	<center>
+			 		<form action="registry.php" method="post">
+			 			<label for="username" class="form-label">Felhasználónév</label>
+			 			<input id="username" name="username" class="form-control "type="text">
+			 			<label for="password" class="form-label">Jelszó</label>
+			 			<input id="password" name="password" class="form-control "type="password">
+			 			<button type="submit" name="registry" class="btn btn-primary">Regisztrálok</button>
+			 		</form>
+			 	</center>
+			 </div>
 			');
 	}
 
 	function __destruct()
-	{
+	{	
 		echo('
 			</body>
-			</html>
+			</htlm>
 			');
 	}
+
 }
+
 
 ?>
